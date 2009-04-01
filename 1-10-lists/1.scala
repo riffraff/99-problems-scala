bc4f65fbb752ccba2835e1f8dede844a1f22dbe6
gabriele renzi <rff.rff@gmail.com>
2009-04-02 00:04:41 +0200

    list one

---
+++ 1-10-lists/1.scala
@@ -0,0 +1,11 @@
+//(*) Find the last element of a list. 
+
+val x= List(1,2,3,4);
+def myLast[T](list :List[T]):T = list match {
+  case List()      => error("this list is empty")
+  case x::List() => x
+  case _::xs     => myLast(xs)
+}
+println(myLast(x)==4)
+
+println(myLast2(x)==4)
