8072f53b8b1a801cc260659c0e1dc266d75bbe86
gabriele renzi <rff.rff@gmail.com>
2009-04-05 12:12:19 +0200

    couple more ways of writing last

--- 1-10-lists/1.scala
+++ 1-10-lists/1.scala
@@ -2,9 +2,17 @@
 
 val x= List(1,2,3,4);
 def myLast[T](list :List[T]):T = list match {
-  case List()      => error("this list is empty")
-  case x::List() => x
+  case Nil      => error("this list is empty")
+  case x::Nil => x
   case _::xs     => myLast(xs)
 }
+
+def myLast2[T](list : List[T]): T = list.last
+
+def myLast3[T](list : List[T]): T = list.reverse.head
+def myLast4[T](list : List[T]): T = list(list.size-1)
 println(myLast(x)==4)
+println(myLast2(x)==4)
+println(myLast3(x)==4)
+println(myLast4(x)==4)
 
