515c72582edce13eec9adb9359e135c699425b78
gabriele renzi <rff.rff@gmail.com>
2009-04-05 13:52:39 +0200

    5, reverse

---
+++ 1-10-lists/5.scala
@@ -0,0 +1,14 @@
+// reverse a list
+
+val x= List(1,2,3,4);
+val y= List(4,3,2,1);
+
+def myReverse[T](list:List[T]):List[T] = list.reverse
+def myReverse2[T](list:List[T]):List[T] = list match {
+    case Nil=>Nil
+    case x::Nil=>List(x)
+    case x::xs => myReverse2(xs) ::: List(x)
+}
+
+println(myReverse(x)==y)
+println(myReverse2(x)==y)
