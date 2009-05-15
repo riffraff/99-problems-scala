665c026583228d849ded82835a8dd06bf5192d90
gabriele renzi <rff.rff@gmail.com>
2009-05-15 17:02:03 +0200

    add note about List.flatten

--- 1-10-lists/7.scala
+++ 1-10-lists/7.scala
@@ -9,7 +9,13 @@ def flatten(list:List[Any]):List[Any] =
       case x: List[Any] => flatten(x)
       case x  => List(x)
     })
+val flatten2 = List.flatten _ 
 
 println(flatten(List(List(1))))
 println(flatten(List(0, 1,List(2,List(3),List()))))
 println(flatten(List(1, List(2, List(3,4), 5))))
+
+println(flatten2(List(List(1))))
+//println(flatten2(List(List(0), List(1),List(2))))
+// lists must be uniform
+//println(flatten2(List(List(0), List(1),List(List(2)))))
--- 1-10-lists/8.scala
+++ 1-10-lists/8.scala
@@ -6,8 +6,10 @@ If a list contains repeated elements they should be replaced with a single copy 
 
 def compress[T](list: List[T]): List[T] = 
     list.foldLeft(List(list.head))((acc,el)=> if (acc.last==el) acc else acc++List(el))
+
 def compress2[T](list: List[T]): List[T] = 
     list.foldRight(List(list.last))((el,acc)=> if (acc.head==el) acc else el::acc)
+
 println(compress(List('a','a','a','a','b','c','c','a','a','d','e','e','e','e')))
 println(compress2(List('a','a','a','a','b','c','c','a','a','d','e','e','e','e')))
 //['a','b','c','a','d','e']
