c4601aa1157860bcbcc8e94a49fae08527addea5
gabriele renzi <rff.rff@gmail.com>
2009-05-15 16:12:32 +0200

    7 works clunky

---
+++ 1-10-lists/7.scala
@@ -0,0 +1,17 @@
+/* 
+(**) Flatten a nested list structure.
+
+Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively). 
+*/
+
+def flatten(list:List[Any]):List[Any] =
+  list.flatMap( 
+    x=> x match {
+      case x: List[Any] => flatten(x)
+      case _  => List(x)
+    }
+    )
+
+println(flatten(List(List(1))))
+println(flatten(List(0, 1,List(2,List(3),List()))))
+println(flatten(List(1, List(2, List(3,4), 5))))
