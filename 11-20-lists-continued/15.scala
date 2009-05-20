1074ebb2f8d5e8aacb8e31b3be778791da59c34b
gabriele renzi <rff.rff@gmail.com>
2009-05-20 19:03:16 +0200

    solved 15 with flatMap and range

---
+++ 11-20-lists-continued/15.scala
@@ -0,0 +1,17 @@
+/*
+(**) Replicate the elements of a list a given number of times.
+
+Example:
+* (repli '(a b c) 3)
+(A A A B B B C C C)
+
+Example in Haskell:
+> repli "abc" 3
+"aaabbbccc"
+
+*/
+def repli[T](list :List[T], n: Int): List[T] =
+    list.flatMap(x=>new Range(0,n,1) map {i=>x})
+
+println(repli(List(1,2,3),3))
+// vim: set ts=4 sw=4 et:
