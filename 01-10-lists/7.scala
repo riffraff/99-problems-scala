bf86a88aeac2daac71b235ef24bc464c0a6db06b
gabriele renzi <rff.rff@gmail.com>
2009-05-20 09:51:53 +0200

    recursive solution for flatten

--- 01-10-lists/7.scala
+++ 01-10-lists/7.scala
@@ -11,11 +11,21 @@ def flatten(list:List[Any]):List[Any] =
     })
 val flatten2 = List.flatten _ 
 
+def flatten3(list: List[Any]): List[Any] =    list match {
+  case (x: List[Any]) :: xs => flatten(x) ::: flatten(xs)
+  case x :: xs => x :: flatten(xs) 
+  case Nil => Nil 
+}
+
+
 println(flatten(List(List(1))))
 println(flatten(List(0, 1,List(2,List(3),List()))))
 println(flatten(List(1, List(2, List(3,4), 5))))
 
-println(flatten2(List(List(1))))
+//println(flatten2(List(List(1))))
 //println(flatten2(List(List(0), List(1),List(2))))
 // lists must be uniform
 //println(flatten2(List(List(0), List(1),List(List(2)))))
+println(flatten3(List(List(1))))
+println(flatten3(List(0, 1,List(2,List(3),List()))))
+println(flatten3(List(1, List(2, List(3,4), 5))))
